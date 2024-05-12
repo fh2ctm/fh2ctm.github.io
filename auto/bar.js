617d7effb42c50f7e85dfe3a75a1681abb4ecd81
Rex Fang <fh2ctm@gmail.com>
2024-05-12 12:31:05 -0400

    Refactoring and automated left bar generation

---
+++ auto/bar.js
@@ -0,0 +1,21 @@
+const filenameToEntry = {
+  "index": "About",
+  "code": "Code",
+  "courses-taken": "Courses Taken",
+  "lang": "Language",
+  "math": "Math",
+}
+
+function makeLeftBar() {
+  const leftBar = document.getElementsByClassName("leftbar")[0];
+  const currentFilename = document.getElementsByTagName("title")[0].attributes["filename"].value;
+  for (const [filename, entry] of Object.entries(filenameToEntry)) {
+    const newLink = document.createElement("a");
+    newLink.setAttribute("href", filename + ".html");
+    newLink.innerHTML = entry;
+    if (filename == currentFilename) {
+      newLink.setAttribute("selected", "");
+    }
+    leftBar.appendChild(newLink);
+  }
+}
