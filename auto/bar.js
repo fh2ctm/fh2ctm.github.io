7dea70b7cfc2e50058616b67442f5eaa6a0b6ee9
Rex Fang <fh2ctm@gmail.com>
2024-05-12 22:12:14 -0400

    Adjusted page structure

--- auto/bar.js
+++ auto/bar.js
@@ -1,34 +1,32 @@
 const pageCategory = {
-  Personal: {
-    index: "About",
-  },
-  Code: {
-    code: "Code",
-  },
   Academic: {
     "courses-taken": "Courses Taken",
   },
 };
 
-function makeLeftBar() {
+function makeLeftBar(forIndex=false) {
   const leftBar = document.getElementsByClassName("leftbar")[0];
   for (const [category, pages] of Object.entries(pageCategory)) {
     const categoryHeader = document.createElement("span");
     categoryHeader.innerHTML = category;
     leftBar.appendChild(categoryHeader);
-    makeLeftBarWithObject(pages);
+    makeLeftBarWithObject(pages, forIndex);
     const divider = document.createElement("span");
     divider.innerHTML = "\u00A0";
     leftBar.appendChild(divider);
   }
 }
 
-function makeLeftBarWithObject(obj) {
+function makeLeftBarWithObject(obj, forIndex=false) {
   const leftBar = document.getElementsByClassName("leftbar")[0];
   const currentFilename = document.getElementsByTagName("title")[0].attributes["filename"].value;
   for (const [filename, entry] of Object.entries(obj)) {
     const newLink = document.createElement("a");
-    newLink.setAttribute("href", filename + ".html");
+    if (forIndex) {
+      newLink.setAttribute("href", "pages/" + filename + ".html");
+    } else {
+      newLink.setAttribute("href", filename + ".html");
+    }
     newLink.innerHTML = entry;
     if (filename == currentFilename) {
       newLink.setAttribute("selected", "");
