254f9eec0fe698cbaf22748f4908da382b124e50
Rex Fang <fh2ctm@gmail.com>
2024-05-16 16:28:39 -0400

    Automated right bar generation

--- gen/rightbar.js
+++ gen/rightbar.js
@@ -1,2 +1,40 @@
 import { NewNode } from '/gen/meta.js';
 
+/* <div class="leftbar"></div> */
+function RightBar() {
+  return NewNode("div", {
+    "class": "rightbar"
+  })
+}
+
+/* <span divider>&nbsp;</span> */
+function Divider(text = "\u00A0") {
+  return NewNode("span", {}, text);
+}
+
+/* <a href="#id">text</a> */
+function Link(id, text) {
+  let localPath = "# " + id;
+  return NewNode("a", {
+    "href": localPath
+  }, text)
+}
+
+export default function makeRightBar(headingObj) {
+  let rightbar = RightBar();
+  Object.entries(headingObj).forEach(([section, headings]) => {
+    rightbar.appendChild(Divider(section));
+    if (Array.isArray(headings)) {
+      headings.entries().forEach((heading) => {
+        let id = heading.replace(/\s/g, "").toLowerCase();
+        rightbar.appendChild(Link(id, heading));
+      })
+    } else {
+      Object.entries(headings).forEach(([id, heading]) => {
+        rightbar.appendChild(Link(id, heading));
+      });
+    }
+    rightbar.appendChild(Divider());
+  })
+  document.body.appendChild(rightbar);
+}
