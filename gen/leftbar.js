044bbc1dd02712846ac0af38fdf60f0f33ac0d30
Rex Fang <fh2ctm@gmail.com>
2024-05-16 08:50:18 -0400

    Restructured

--- gen/leftbar.js
+++ gen/leftbar.js
@@ -2,32 +2,30 @@ const pageCategory = {
   Academic: {
     "courses-taken": "Courses Taken",
   },
+  Language: {
+    "qeerty": "QÉERTY Keyboard"
+  }
 };
 
-function makeLeftBar(forIndex=false) {
+function makeLeftBar() {
   const leftBar = document.getElementsByClassName("leftbar")[0];
   for (const [category, pages] of Object.entries(pageCategory)) {
     const categoryHeader = document.createElement("span");
     categoryHeader.innerHTML = category;
     leftBar.appendChild(categoryHeader);
-    makeLeftBarWithObject(pages, forIndex);
+    makeLeftBarWithObject(pages);
     const divider = document.createElement("span");
     divider.innerHTML = "\u00A0";
     leftBar.appendChild(divider);
   }
-  makeFooter();
 }
 
-function makeLeftBarWithObject(obj, forIndex=false) {
+function makeLeftBarWithObject(obj) {
   const leftBar = document.getElementsByClassName("leftbar")[0];
   const currentFilename = document.getElementsByTagName("title")[0].attributes["filename"].value;
   for (const [filename, entry] of Object.entries(obj)) {
     const newLink = document.createElement("a");
-    if (forIndex) {
-      newLink.setAttribute("href", "pages/" + filename + ".html");
-    } else {
-      newLink.setAttribute("href", filename + ".html");
-    }
+    newLink.setAttribute("href", filename + ".html");
     newLink.innerHTML = entry;
     if (filename == currentFilename) {
       newLink.setAttribute("selected", "");
