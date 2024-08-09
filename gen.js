2abbe5f319c1adbab04316dbdadd44c6df5ca018
Rex Fang <fh2ctm@gmail.com>
2024-08-09 17:03:52 -0400

    Retab

--- gen.js
+++ gen.js
@@ -6,37 +6,37 @@
  * @returns {Element} A new HTMLElement.
  */
 function NewNode(tag = "badtag", attr = null, text = "") {
-  let newNode = document.createElement(tag);
-  if (attr) {
-    if (Array.isArray(attr)) {
-      attr.entries().forEach((attrKey) => {
-        newNode.setAttribute(attrKey, "");
-      });
-    } else {
-      Object.entries(attr).forEach(([attrKey, attrStr]) => {
-        newNode.setAttribute(attrKey, attrStr);
-      });
-    }
-  }
-  newNode.innerText = text;
-  return newNode;
+	let newNode = document.createElement(tag);
+	if (attr) {
+		if (Array.isArray(attr)) {
+			attr.entries().forEach((attrKey) => {
+				newNode.setAttribute(attrKey, "");
+			});
+		} else {
+			Object.entries(attr).forEach(([attrKey, attrStr]) => {
+				newNode.setAttribute(attrKey, attrStr);
+			});
+		}
+	}
+	newNode.innerText = text;
+	return newNode;
 }
 
 /**
  * Layout of the page structure. 
  */
 const pageCategory = {
-  Academic: {
-    "courses-taken": "Courses Taken",
-    "research": "Research",
-  },
-  Fun: {
-    "blog": "Blog",
-  },
-  Language: {
-    "cn-ime": "Chinese IME",
-    "qeerty": "QÉERTY Keyboard",
-  },
+	Academic: {
+		"courses-taken": "Courses Taken",
+		"research": "Research",
+	},
+	Fun: {
+		"blog": "Blog",
+	},
+	Language: {
+		"cn-ime": "Chinese IME",
+		"qeerty": "QÉERTY Keyboard",
+	},
 };
 
 /**
@@ -45,72 +45,65 @@ const pageCategory = {
  * @returns {Element} Divider.
  */
 function Divider(text = "\u00A0") {
-  return NewNode("span", null, text);
+	return NewNode("span", null, text);
 }
 
-/**
- * Make a left bar using page category.
- */
-function makeLeftBar() {
-  let filename = document.querySelector("head title").getAttribute("filename") || "";
-  let leftBar = document.querySelector("nav.left");
-  leftBar.appendChild(NewNode(
-    "a",
-    {
-      signature: "",
-      href: "/index.html",
-    },
-    "Rex Fang",
-  ));
-  leftBar.appendChild(Divider());
-  Object.entries(pageCategory).forEach(([category, pages]) => {
-    leftBar.appendChild(Divider(category));
-    Object.entries(pages).forEach(([title, page]) => {
-      let pageDir = "/pages/" + category.toLowerCase() + "/" + title + ".html";
-      let link = NewNode("a", { href: pageDir }, page);
-      if (filename == title) link.setAttribute("selected", "");
-      leftBar.appendChild(link);
-    });
-    leftBar.appendChild(Divider());
-  });
-}
+// make a leftbar
 
-/**
- * Make a right bar by listing all levels 1, 2, 3 headings with id.
- */
-function makeRightBar() {
-  let rightbar = document.querySelector("nav.right");
-  rightbar.appendChild(NewNode(
-    "a",
-    {
-      signature: "",
-      href: "#top",
-    },
-    "Go Top",
-  ));
-  rightbar.appendChild(Divider());
-  let seenH1 = false;
-  document.body.querySelectorAll("h1[id], h2[id]").forEach((heading) => {
-    let hDepth = heading.tagName.charAt(1);
-    if (hDepth === "1") {
-      if (seenH1) {
-        rightbar.appendChild(Divider());
-      } else {
-        seenH1 = true;
-      }
-    }
-    let label = heading.getAttribute("label");
-    rightbar.appendChild(NewNode(
-      "a",
-      {
-        class: "h" + hDepth,
-        href: "#" + heading.id,
-      },
-      label ? label : heading.innerText,
-    ));
-  });
-  rightbar.appendChild(Divider());
-}
+let filename = document.querySelector("head title").getAttribute("filename") || "";
+let leftBar = document.querySelector("nav.left");
+leftBar.appendChild(NewNode(
+	"a",
+	{
+		signature: "",
+		href: "/index.html",
+	},
+	"Rex Fang",
+));
+leftBar.appendChild(Divider());
+Object.entries(pageCategory).forEach(([category, pages]) => {
+	leftBar.appendChild(Divider(category));
+	Object.entries(pages).forEach(([title, page]) => {
+		let pageDir = "/pages/" + category.toLowerCase() + "/" + title + ".html";
+		let link = NewNode("a", { href: pageDir }, page);
+		if (filename == title) link.setAttribute("selected", "");
+		leftBar.appendChild(link);
+	});
+	leftBar.appendChild(Divider());
+});
 
-makeLeftBar();
-if (document.querySelector("nav.right")) makeRightBar();
+// Make a right bar by listing all levels 1, 2, 3 headings with id.
+
+if (document.querySelector("nav.right")) {
+	let rightbar = document.querySelector("nav.right");
+	rightbar.appendChild(NewNode(
+		"a",
+		{
+			signature: "",
+			href: "#top",
+		},
+		"Go Top",
+	));
+	rightbar.appendChild(Divider());
+	let seenH1 = false;
+	document.body.querySelectorAll("h1[id], h2[id]").forEach((heading) => {
+		let hDepth = heading.tagName.charAt(1);
+		if (hDepth === "1") {
+			if (seenH1) {
+				rightbar.appendChild(Divider());
+			} else {
+				seenH1 = true;
+			}
+		}
+		let label = heading.getAttribute("label");
+		rightbar.appendChild(NewNode(
+			"a",
+			{
+				class: "h" + hDepth,
+				href: "#" + heading.id,
+			},
+			label ? label : heading.innerText,
+		));
+	});
+	rightbar.appendChild(Divider());
+}
