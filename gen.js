d16be7a8adec30a0f09355283b8775a00f1e1bca
Rex Fang <fh2ctm@gmail.com>
2024-08-22 21:41:56 -0400

    cleanup

--- gen.js
+++ gen.js
@@ -8,15 +8,9 @@
 function NewNode(tag = "badtag", attr = null, text = "") {
 	let newNode = document.createElement(tag);
 	if (attr) {
-		if (Array.isArray(attr)) {
-			attr.entries().forEach((attrKey) => {
-				newNode.setAttribute(attrKey, "");
-			});
-		} else {
-			Object.entries(attr).forEach(([attrKey, attrStr]) => {
-				newNode.setAttribute(attrKey, attrStr);
-			});
-		}
+		Object.entries(attr).forEach(([attrKey, attrStr]) => {
+			newNode.setAttribute(attrKey, attrStr);
+		});
 	}
 	newNode.innerText = text;
 	return newNode;
